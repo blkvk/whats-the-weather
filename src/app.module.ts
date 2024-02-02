131c319b18989d40eae725fd2ac20c2ff54cd3d5
blkvk <kabeliakov@gmail.com>
2024-02-02 14:06:20 +0300

    fix: settings and some bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,37 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { SequelizeModule } from "@nestjs/sequelize";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { WeatherModule } from './weather/weather.module';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             envFilePath: '.env'
         }),
-        SequelizeModule.forRoot({
-            dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: '',
-            database: 'weather',
-            models: [],
-            autoLoadModels: true
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            entities: [__dirname + '/**/*.entity{.js, .ts}'],
+            synchronize: true,
+            namingStrategy: new SnakeNamingStrategy()
           }),
-  ],})
+        UsersModule,
+        AuthModule,
+        WeatherModule,
+        HttpModule.registerAsync({
+          useFactory: () => ({
+            timeout: 120000,
+            maxRedirects: 5,
+          }),
+        }),
+        HttpModule
+  ]})
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function start() {
     const PORT = process.env.PORT || 5005;
     const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+
+    const config = new DocumentBuilder()
+        .setTitle('Какая сейчас погода?')
+        .setDescription('Документация REST API')
+        .setVersion('1.0')
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('/api/docs', app, document)
 
     await app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
 }
